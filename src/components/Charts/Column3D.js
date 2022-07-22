90405ed3668ceefc0e3dc4ae139625dfb6c67c04
kyawkhaing <kyawkhaing29581@gmail.com>
2022-07-22 21:40:35 +0630

    github search users project

---
+++ src/components/Charts/Column3D.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+// Resolves charts dependancy
+
+const Column3D = ({ data }) => {
+  const chartConfigs = {
+    type: "column3d",
+    width: "100%",
+    height: 400,
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Most Popular",
+        yAxisName: "Stars",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+      },
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default Column3D;
---
+++ src/components/Charts/Doughnut2d.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+// Resolves charts dependancy
+
+const Doughnut2D = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d",
+    width: "100%",
+    height: 400,
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Stars Per Language",
+        theme: "candy",
+        decimals: 0,
+        doughnutRadius: "45%",
+        showPercentValues: 0,
+      },
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default Doughnut2D;
